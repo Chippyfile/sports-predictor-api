09411c93eac1154aac32221b80dbadc66ed997a7
Chip Peterson <chippyfile@gmail.com>
2026-03-04 19:44:55 -0800

    MLB Backtest Fix

--- backtests.py
+++ backtests.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from sports.mlb import mlb_build_features
+from sports.mlb import mlb_build_features,  backfill_heuristic
 from sports.nba import nba_build_features
 from sports.ncaa import ncaa_build_features
 import numpy as np, pandas as pd, traceback, shap
